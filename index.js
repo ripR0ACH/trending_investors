998f2e015536bd139bbc0da21ce914926e8f6ce6
zach morrison <morrisonizach@gmail.com>
2020-12-14 08:47:46 -0600

    messing with a lot of css

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 $(document).ready(function () {
+    checkSize();
+    $(window).resize(checkSize);
     $('#nav-toggle').click(function () {
         $(this).toggleClass('open');
+        $('.nav, .content').toggleClass('open');
     });
 });
+function checkSize() {
+    if ($('.nav').css("width") == "20vw") {
+        $('#nav-toggle, .nav, .content').toggleClass('open');
+    }
+}
