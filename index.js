ae3134902d88cf0817684cc46a49c26d06ef543f
zach morrison <morrisonizach@gmail.com>
2020-12-15 05:29:15 -0600

    getting off for the night

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 $(document).ready(function () {
-    checkSize();
-    $(window).resize(checkSize);
     $('#nav-toggle').click(function () {
         $(this).toggleClass('open');
         $('.nav, .content').toggleClass('open');
     });
 });
-function checkSize() {
-    if ($('.nav').css("width") == "20vw") {
-        $('#nav-toggle, .nav, .content').toggleClass('open');
-    }
-}
